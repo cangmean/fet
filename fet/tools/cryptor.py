35a16d1b843a1034b88c19dccffd2cb8ffdfe707
changmin.jin <changmin.jin@17zuoye.comm>
2019-12-10 15:21:45 +0800

    fix

--- fet/tools/cryptor.py
+++ fet/tools/cryptor.py
@@ -1,7 +1,12 @@
+import base64
 import functools
+from binascii import a2b_hex
+from binascii import b2a_hex
+
 from Crypto.Cipher import AES
-from binascii import b2a_hex, a2b_hex
-from fet.utils import to_bytes, to_string
+
+from fet.utils import to_bytes
+from fet.utils import to_string
 
 
 def wrap_encode(input_func=None, output_func=None):
@@ -28,11 +33,11 @@ class Cryptor(object):
         self._SECRET_KEY = None
         if app is not None:
             self.init_app(app)
-    
+
     def init_app(self, app):
         if not hasattr(app, 'extensions'):
             app.extensions = {}
-        
+
         app.config.setdefault(
             'FET_AES_SECRET_KEY',
             'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -41,7 +46,7 @@ class Cryptor(object):
         app.extensions['fet_cryptor'] = self
         if not self._SECRET_KEY:
             self._SECRET_KEY = to_bytes(app.config['FET_AES_SECRET_KEY'])
-    
+
     @wrap_encode(input_func=to_bytes)
     def ensure_key(self, text):
         """ 有效的密钥"""
@@ -49,7 +54,7 @@ class Cryptor(object):
         add = length - (count % length)
         text += b'\0' * add
         return text
-    
+
     @wrap_encode(to_bytes, to_string)
     def encrypt(self, text, salt=None):
         """ 加密"""
@@ -58,7 +63,7 @@ class Cryptor(object):
         crypt = AES.new(self._SECRET_KEY[:16], AES.MODE_CBC, salt)
         plain_text = crypt.encrypt(text)
         return b2a_hex(plain_text)
-    
+
     @wrap_encode(to_bytes, to_string)
     def decrypt(self, text, salt=None):
         """ 解密"""
@@ -67,3 +72,36 @@ class Cryptor(object):
         plain_text = crypt.decrypt(a2b_hex(text))
         plain_text = plain_text.rstrip(b'\0')
         return plain_text
+
+
+class _AES(object):
+
+    def __init__(self, key, iv, mod=AES.MODE_CBC):
+        self.key = to_bytes(key[:16])
+        self.iv = to_bytes(iv)
+        self.mod = mod
+
+    def __pad(self, text):
+        text_length = len(text)
+        amount_to_pad = AES.block_size - (text_length % AES.block_size)
+        if amount_to_pad == 0:
+            amount_to_pad = AES.block_size
+        return text + bytes([amount_to_pad]) * amount_to_pad
+
+    def __unpad(self, text):
+        pad = ord(text[-1])
+        return text[:-pad]
+
+    def encrypt(self, raw):
+        """加密"""
+        raw = to_bytes(raw)
+        raw = self.__pad(raw)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return base64.b64encode(cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        """解密"""
+        enc = to_bytes(enc)
+        enc = base64.b64decode(enc)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return self.__unpad(to_string(cipher.decrypt(enc)))
